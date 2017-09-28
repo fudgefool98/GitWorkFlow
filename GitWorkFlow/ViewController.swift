87eb6a3c0610ec93cd7134398dae2f87f7ed1e18
Jordan Liebman <jordanliebman@mwc-053020.dhcp.missouri.edu>
2017-09-28 13:39:37 -0500

    added farts to line 12

--- GitWorkFlow/ViewController.swift
+++ GitWorkFlow/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-//awesome programming
+//awesome programming farts
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
